60f5ec2d5d1320128d6715124dc7e0b50921378b
Jihoo Byeon <jihoobyeon@gmail.com>
2022-06-11 02:01:00 +0900

    add parenthesis

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //import { createReactNativeComponentClass } from 'react-native-macos';
 const createReactNativeComponentClass = require('react-native/Libraries/Renderer/shims/createReactNativeComponentClass');
 
-const NativeRNTouchbar = createReactNativeComponentClass('TouchBar', () => {
-  uiViewClassName: 'RNTouchbar'
-});
+const NativeRNTouchbar = createReactNativeComponentClass('TouchBar', () => ({
+   validAttributes: {},
+   uiViewClassName: 'RNTouchbar'
+ }));
 
 export default NativeRNTouchbar;
