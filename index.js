d057efef168e4f07f786bf48cfab65bd965181ca
Jihoo Byeon <jihoobyeon@gmail.com>
2022-06-12 13:13:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const createReactNativeComponentClass = require('react-native/Libraries/Renderer/shims/createReactNativeComponentClass');
 
-const NativeRNTouchbar = createReactNativeComponentClass('RNTouchbar', () => ({
+const NativeRNTouchbar = createReactNativeComponentClass('TouchBar', () => ({
    validAttributes: {},
    uiViewClassName: 'RNTouchbar'
  }));
