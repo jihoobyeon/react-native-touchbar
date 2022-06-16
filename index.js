643ea19ee415b3ee7ee6458bda09a03012d5ae2e
Jihoo Byeon <jihoobyeon@gmail.com>
2022-06-16 22:01:48 +0900

    Update and rename index.js to TouchBar.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const createReactNativeComponentClass = require('react-native/Libraries/Renderer/shims/createReactNativeComponentClass');
-
-const NativeRCTTouchBar = createReactNativeComponentClass('RCTTouchBar', () => ({
-   validAttributes: {},
-   uiViewClassName: 'RCTTouchBar'
- }));
-
-export default NativeRCTTouchBar;
