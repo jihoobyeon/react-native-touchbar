a51eb53064f6ce16fd4b67404cf7808d68270fc6
Jihoo Byeon <jihoobyeon@gmail.com>
2022-06-19 22:21:06 +0900

    Update RCTTouchBar.m

--- RCTTouchBar.m
+++ RCTTouchBar.m
@@ -47,7 +47,7 @@ static void * TouchBarItemsKey = &TouchBarItemsKey;
     if (!self.touchBarItems || self.touchBarItems.count == 0) {
         return nil;
     }
-    NSCustomTouchBarItem __block *toRender = nil;
+    NSCustomTouchBarItem *customItemForLabel = nil;
     [self.touchBarItems enumerateObjectsUsingBlock:^(id touchItem, NSUInteger index, BOOL *stop) {
         if (((NSCustomTouchBarItem *)touchItem).view.reactTag.stringValue == identifier) {
             toRender = touchItem;
